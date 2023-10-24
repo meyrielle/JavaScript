9486b7afa5b4edb0855c9e94451af4ee5b27a46b
meyrielle <126573606+meyrielle@users.noreply.github.com>
2023-10-24 10:58:40 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document. querySelectorAll(`.tecla`);
+}
 
-document.querySelector("#som_tecla_pom").play();
+const lisstaDeTeclas = document. querySelectorAll(`.tecla`);
+
+//para
+for(let contador= 0; contador < lisstaDeTeclas.length; contador++){
+
+const tecla = lisstaDeTeclas [contador];
+const instrumento = tecla.classList[1]
+const idAudio = `#som ${instrumento}`; //template string
+
+tecla.onclick = function(){
+tocaSom(idAudio);
+}
+}
+
